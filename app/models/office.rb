25db29ffcffc1808f52ec9636915c2f93ef6d16c
vibraniumforge <gregm9999@gmail.com>
2019-11-20 10:03:44 -0800

    Done.

--- app/models/office.rb
+++ app/models/office.rb
@@ -2,4 +2,15 @@ class Office < ApplicationRecord
   belongs_to :building
   belongs_to :company
   validates :floor, numericality: { only_integer: true }
+
+  def num_employees
+    self.company.employees.count
+  end
+
+  def list_employees
+    self.company.employees.pluck("name", "title").map do |emp|
+      "Name: #{emp[0]}, Title: #{emp[1]}"
+    end
+  end
+
 end
